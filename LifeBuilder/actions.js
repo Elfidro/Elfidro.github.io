df3de83f4ca788498bdffb8373bed73c9d8fc8e1
Elfidro <luigi.cortez@gmail.com>
2023-10-10 15:11:39 -0300

    Updated website and Life Builder changes can be seen in devlog 10/10/2023

--- LifeBuilder/actions.js
+++ LifeBuilder/actions.js
@@ -3,49 +3,59 @@ let isWorking = false;
 
 var gameState = {
     money: 0,
-    level: 5,
+    jobProgress: {
+        'beggar': { progress: 0, level: 0, income: 10 },
+        'recycler': { progress: 0, level: 0, income: 20 },
+        'performer': { progress: 0, level: 0, income: 30 },
+        'oddjobsworker': { progress: 0, level: 0, income: 40 },
+        'daylabourer': { progress: 0, level: 0, income: 50 },
+    },
     // Add more data as needed
 };
 
 // Define an object to keep track of progress for each job
-let jobProgress = {
-    'beggar': 0,
-    'recycler': 0,
-    'performer': 0,
-    'oddjobsworker': 0,
-    'daylabourer': 0
-};
 
 function toggleWork(job) {
-    const progress = document.getElementById(`${job}-progress`);
-    //const isActive = progress.classList.contains("active");
+    const jobData = gameState.jobProgress[job];
 
     if (isWorking) {
         clearInterval(workInterval);
-        //progress.classList.remove("active");
         isWorking = false;
         toggleWork(job);
     } else {
         workInterval = setInterval(function () {
             // Increment work by value
-            //workProgress += 10;
-            jobProgress[job] += 1 * !game.paused;
+            jobData.progress += 5 * !game.paused;
 
             // Increase progress bar by %
-            //progress.style.width = workProgress + '%';
-            document.getElementById(job + '-progress').style.width = jobProgress[job] + '%';
+            document.getElementById(job + '-progress').style.width = jobData.progress + '%';
 
             // Checks if bar is full, if true then execute code
-            if (jobProgress[job] >= 100) {
-                jobProgress[job] = 0;                                                     // Reset progress bar to 0%
-                gameState.money += 470;                                                    // Increment money by 10
+            if (jobData.progress >= 100) {
+                jobData.progress = 0;                                                     // Reset progress bar to 0%
+                gameState.money += jobData.income;                                        // Increment money by income
+                jobData.level++;
+                updateRows();
                 checkBuyTent();                                                           // Check if user can make purchases
                 document.getElementById('money').innerText = gameState.money;             // Paste new value of 'money' to html
             }
         }, 100);
-        //progress.classList.add("active");
         isWorking = true;
     }
+}
+
+// Iterate over the rows and update the income value
+function updateRows() {
+    const rows = document.querySelectorAll('#jobs tr[id^="row"]');
+    rows.forEach(row => {
+        const jobType = row.id.slice(4).toLowerCase(); // Extract job type from row ID
+        const levelElement = row.querySelector('.level');
+        const incomeElement = row.querySelector('.income');
+        if (gameState.jobProgress[jobType]) {
+            levelElement.textContent = gameState.jobProgress[jobType].level;
+            incomeElement.textContent = gameState.jobProgress[jobType].income;
+        }
+    });
 }
 
 /* Reincarnation Code */
@@ -81,29 +91,4 @@ for (let i = 0; i < progressBars.length; i++) {
     });
 }
 
-// Save game state
-function saveGame() {
-    localStorage.setItem('gameState', JSON.stringify(gameState));
-    console.log("Saving");
-}
-
-// Reset game State
-function resetGame() {
-    if (confirm("Are you sure you want to reset? This action cannot be undone.")) {
-        localStorage.clear();
-        location.reload();
-    }
-}
-
-// Load game state
-function loadGame() {
-    const gameStateString = localStorage.getItem('gameState');
-
-    console.log("Loading");
-    if (gameStateString) {
-        var gameSave = JSON.parse(gameStateString)
-        console.log("Loading2");
-        document.getElementById('money').innerText = gameSave.money;
-        gameState = gameSave;
-    }
-}
+updateRows();
--- LifeBuilder/settings.js
+++ LifeBuilder/settings.js
@@ -3,10 +3,46 @@ document.getElementById('dark-mode-checkbox').addEventListener('change', functio
     document.body.classList.toggle('dark');
 });
 
-document.getElementById('debug-checkbox').addEventListener('change', function () {
-    var jobs = document.getElementsByClassName('hidden');
+/* Debug mode */
+//document.getElementById('debug-checkbox').addEventListener('change', function () {
+//    var jobs = document.getElementsByClassName('hidden');
+//    const length = jobs.length;
+//    for (var i = 0; i < length; i++) {
+//        jobs[0].classList.remove('hidden');
+//    }
+//});
 
+document.getElementById('debug-checkbox').addEventListener('change', function () {
+    var jobs = Array.from(document.getElementsByClassName('hidden'));
     for (var i = 0; i < jobs.length; i++) {
         jobs[i].classList.remove('hidden');
     }
 });
+
+
+// Save game state
+function saveGame() {
+    localStorage.setItem('gameState', JSON.stringify(gameState));
+    console.log("Saving");
+}
+
+// Reset game State
+function resetGame() {
+    if (confirm("Are you sure you want to reset? This action cannot be undone.")) {
+        localStorage.clear();
+        location.reload();
+    }
+}
+
+// Load game state
+function loadGame() {
+    const gameStateString = localStorage.getItem('gameState');
+
+    console.log("Loading");
+    if (gameStateString) {
+        var gameSave = JSON.parse(gameStateString)
+        console.log("Loading2");
+        document.getElementById('money').innerText = gameSave.money;
+        gameState = gameSave;
+    }
+}
